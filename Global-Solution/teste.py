ccc24317bba32e60fb98972aaca8e8ff09d9c2a3
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-11-20 01:09:23 -0300

    teste.py

--- Global-Solution/teste.py
+++ Global-Solution/teste.py
@@ -2,26 +2,26 @@ import folium
 from api import geolocalizacao
 import json
 
-with open("Global-Solution/json/outras.json", "r", encoding="latin-1") as file:
-    data = json.load(file)
+#https://www.hapvida.com.br/site/rede-exclusiva
 
-# Coordenadas do usuário
-latitude_usuario, longitude_usuario = geolocalizacao.coordenadas_usuario("02346-000", "158")
+def localizacoes_outras():
+    with open("Global-Solution/json/outras.json", "r", encoding="latin-1") as file:
+        data = json.load(file)
 
-# Criar o mapa inicial com as coordenadas do usuário
-mapa = folium.Map(location=[latitude_usuario, longitude_usuario], zoom_start=10)
+    latitude_usuario, longitude_usuario = geolocalizacao.coordenadas_usuario("02346-000", "158")
 
-# Adicionar marcador para a localização do usuário
-folium.Marker([latitude_usuario, longitude_usuario], popup='Minha Localização').add_to(mapa)
+    mapa = folium.Map(location=[latitude_usuario, longitude_usuario], zoom_start=10)
 
-# Adicionar marcadores para as clínicas
-for d in data["outras"]:
-    latitude, longitude = geolocalizacao.coordenadas_hospitais(d)
-    
-    # Verificar se latitude e longitude são válidas antes de adicionar o marcador
-    if latitude is not None and longitude is not None:
-        folium.Marker([latitude, longitude], popup=d["nome"]).add_to(mapa)
-    else:
-        print(f"Coordenadas inválidas para {d['nome']}")
+    folium.Marker([latitude_usuario, longitude_usuario], popup='Minha Localização').add_to(mapa)
 
-mapa.show_in_browser()
+    for d in data["outras"]:
+        latitude, longitude = geolocalizacao.coordenadas_hospitais(d)
+        
+        if latitude is not None and longitude is not None:
+            folium.Marker([latitude, longitude], popup=d["nome"]).add_to(mapa)
+        else:
+            print(f"Coordenadas inválidas para {d['nome']}")
+
+    mapa.show_in_browser("outras.html")
+
+localizacoes_outras()
