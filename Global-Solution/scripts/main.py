d60b76bcf309e7acbb6e010abab167f74136c18e
gabrielrodri33 <gabrielrodri333@gmail.com>
2023-11-23 00:03:23 -0300

    Finalizado cadastro de paciente, médico e endereço

--- Global-Solution/scripts/main.py
+++ Global-Solution/scripts/main.py
@@ -49,20 +49,20 @@ def atualizacao_txt(info, info2):
     with open('Global-Solution/archives/event.txt', 'a') as file:
         file.write(texto)
 
-def carregarLista():
-    with open("Global-Solution/json/cadastro.json", "r") as arquivo:
+def carregarLista(mp):
+    with open(f"Global-Solution/json/{mp}.json", "r") as arquivo:
         return json.load(arquivo)
 
-def salvarCredenciais(email, senha):
+def salvarCredenciais(email, senha, mp):
     try:
-        with open("Global-Solution/json/cadastro.json", "r") as file:
+        with open(f"Global-Solution/json/{mp}.json", "r") as file:
             data = json.load(file)
     except FileNotFoundError:
         data = {}
 
     data[email] = senha
 
-    with open("Global-Solution/json/cadastro.json", "w") as arquivo:
+    with open(f"Global-Solution/json/{mp}.json", "w") as arquivo:
         json.dump(data, arquivo)
 
 def pwd():
@@ -82,7 +82,7 @@ def formatarCpf():
     while True:
         cpf = input("CPF: ")
         while len(cpf) != 11:
-            separador('CPF inválido!', 6)
+            separador('CPF inválido!', 7)
             cpf = input(f'Deve conter 11 caracteres\nDigite seu CPF: ').replace('.', '').replace('-', '').replace(' ', '')
 
         cpf = f'{cpf[:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:]}'
@@ -91,10 +91,11 @@ def formatarCpf():
             verifica = False
         else:
             verifica = True
-
-        consulta = cellerecpf.consultaCpf(cpf)
-        if consulta == True and verifica == True:
             break
+
+        # consulta = cellerecpf.consultaCpf(cpf)
+        # if consulta == True and verifica == True:
+        #     break
 
     return cpf
 
@@ -132,15 +133,6 @@ def formatarCell():
         cell = input("Celular (xx)xxxxx-xxxx: ").replace("(","").replace(")", "").replace("-", "").replace(" ", "")
 
     return f'({cell[:2]}) {cell[2:7]}-{cell[7:]}'
-
-def formatarTelefone():
-    tel = input("Telefone fixo xxxx-xxxx: ").replace("-",'').strip()
-
-    while len(tel) != 8:
-        separador("Entrada inválida!", 7)
-        tel = input("\nTelefone fixo xxxx-xxxx: ").replace("-",'').strip()
-
-    return f'{tel[:4]}-{tel[4:]}'
 
 def validaEmail():
     status = False
@@ -217,9 +209,17 @@ def formata_valor():
             separador("Insira um número válido!", 7)
 
 def login_user():
+    clear_console()
     status = False
     c = 0
-    with open("Global-Solution/json/cadastro.json", "r") as file:
+    mp = validacao(2)
+
+    if mp == 1:
+        mp = 'medicos'
+    else:
+        mp = 'clientes'
+
+    with open(f"Global-Solution/json/{mp}.json", "r") as file:
         dados_login = json.load(file)
     
     while status == False:
@@ -253,12 +253,12 @@ def login_user():
                 senha, code = pwd()
                 crud.updatePwd("cliente", "senha", senha, email)
                 
-                with open("Global-Solution/json/cadastro.json", 'r') as arquivo:
+                with open(f"Global-Solution/json/{mp}.json", 'r') as arquivo:
                     dados_login = json.load(arquivo)
 
                 dados_login[email] = code
 
-                with open("Global-Solution/json/cadastro.json", 'w') as arquivo:
+                with open(f"Global-Solution/json/{mp}.json", 'w') as arquivo:
                     json.dump(dados_login, arquivo, indent=2)
 
                 print(f'Valor da chave {email} alterado para {code}.')
@@ -270,7 +270,11 @@ def login_user():
         separador(30,1)
         centralizar("Logado!", 60)
         atualizacao_txt("Usuário logado", email)
-        option = validacao(6)
+        if mp == 'medicos':
+            option = validacao(5)
+        else:
+            option = validacao(6)
+
     senha = ""
     return  option, login, email
 
@@ -298,6 +302,54 @@ def validacao(dado):
                     separador(30, 1)
                     separador("Entrada inválida!", 7)
                     print("Por favor insira um número")
+        case 2:
+            while not status:
+                try:
+                    separador(30, 1)
+                    centralizar("Médico / Paciente", 60)
+                    separador(30, 1)
+                    option = int(input("1- Médico\n2- Paciente\n"))
+
+                    if 1 <= option <= 2:
+                        status = True
+
+                    else:
+                        clear_console()
+                        separador(30, 1)
+                        separador("Entrada inválida!", 7)
+                        print("Por favor escolha uma opção de 1 a 2!")
+                except ValueError:
+                    clear_console()
+                    separador(30, 1)
+                    separador("Entrada inválida!", 7)
+                    print("Por favor insira um número")
+
+        case 3:
+            while not status:
+                try:
+                    option = int(input("Se todas informações estiverem corretas, digite 0.\nCaso contrário digite a opção que deseja mudar\n"))
+
+                    if 0 <= option <=4:
+                        status = True
+
+                    else:
+                        clear_console()
+                        separador(30, 1)
+                        separador("Entrada inválida!", 7)
+                        print("Por favor escolha uma opção de 0 a 4!")
+                except ValueError:
+                    clear_console()
+                    separador(30, 1)
+                    separador("Entrada inválida!", 7)
+                    print("Por favor insira um número")
+        case 4:
+            while not status:
+                try:
+                    option = input("Informações corretas? [S/N]").strip().upper()
+                    if option in ["S", "N"]:
+                        status = True
+                except ValueError as e:
+                    print(f"Error: {e}")
 
     return option
 
@@ -343,6 +395,133 @@ def cadastroEndereco(cpf):
 
     return endereco_cliente
 
+def addDict(dicionario, nome, email, tel_celular, cpf):
+    dicionario = { 
+        "nome": nome,
+        "email": email,
+        "cpf": cpf,
+        "telefone_celular": tel_celular
+    }
+
+    return dicionario
+
+def cadastro_paciente():
+    dados_cliente = {}
+    # clear_console()
+    separador(30, 3)
+    centralizar("Cadastro", 60)
+    separador(30, 3)
+    nome = input("Nome: ").strip().title()
+    email = validaEmail()
+    senha, code = pwd()
+
+    cpf = formatarCpf()
+    tel_celular = formatarCell()
+
+    dados_cliente = addDict(dados_cliente, nome, email, tel_celular, cpf)
+
+    atualizacao_txt("Paciente cadastrado", cpf)
+
+    cadastroEndereco(cpf)
+
+    crud.insert_paciente(cpf, nome, senha, email, tel_celular)
+    salvarCredenciais(email, code, 'clientes')
+
+    update_paciente(dados_cliente, cpf)
+
+
+def update_paciente(dados_cliente, cpf):
+    mudanca = ""
+    while mudanca != 0:
+        clear_console()
+        separador(30, 2)
+        centralizar("Informações de Cadastro", 60)
+        separador(30, 2)
+        print(f'1- Nome: {dados_cliente["nome"]}\n2- Email: {dados_cliente["email"]}\n3- Celular: {dados_cliente["telefone_celular"]}\n')
+        mudanca = validacao(3)
+
+        match mudanca:
+            case 1:
+                dados_cliente["nome"] = input('Nome: ').strip().title()
+                crud.update("paciente", "nome", dados_cliente["nome"], cpf)
+                atualizacao_txt("Nome atualizado", dados_cliente["nome"])
+            case 2:
+                dados_cliente["email"] = validaEmail()
+                crud.update("paciente", "email", dados_cliente["email"], cpf)
+                atualizacao_txt("Email atualizado", dados_cliente["email"])
+
+            case 3:
+                dados_cliente["telefone_celular"] = formatarCell()
+                crud.update("paciente", "telefone", dados_cliente["telefone_celular"], cpf)
+                atualizacao_txt("Celular atualizado!", dados_cliente["telefone_celular"])
+
+    return dados_cliente["email"]
+
+def especialidade():
+    especialidades = [
+        "Oncologia Clínica",
+        "Ortopedia e Traumatologia",
+        "Otorrinolaringologia",
+        "Patologia",
+        "Patologia Clínica",
+        "Patologia Clínica/Medicina Laboratorial",
+        "Pediatria",
+        "Pneumologia",
+        "Pneumologia e Tisiologia",
+        "Proctologia",
+        "Psiquiatria",
+        "Psiquiatria Infantil",
+        "Radiodiagnóstico",
+        "Radiologia",
+        "Radiologia e Diagnóstico por Imagem",
+        "Radioterapia",
+        "Reumatologia",
+        "Sexologia",
+        "Terapia Intensiva",
+        "Terapia Intensiva Pediátrica",
+        "Cuidados Intensivos Pediátricos",
+        "Tisiologia",
+        "Toco-Ginecologia",
+        "Ultrassonografia",
+        "Ultrassonografia em Ginecologia e Obstetrícia",
+        "Ultrassonografia Geral",
+        "Urologia"
+    ]
+    print("Escolha uma especialidade:")
+    for i, especialidade in enumerate(especialidades, start=1):
+        print(f"{i}- {especialidade}")
+
+    return especialidades
+
+def obter_especialidade():
+    especialidades = especialidade()
+    escolha = int(input("Digite o número da especialidade desejada: "))
+    
+    if 1 <= escolha <= len(especialidades):
+        return escolha
+    else:
+        print("Opção inválida. Por favor, escolha um número válido.")
+        return obter_especialidade(especialidades)
+
+def cadastro_medico():
+    separador(30, 3)
+    centralizar("Cadastro médico", 60)
+    separador(30, 3)
+    crm = input("CRM: ")
+    nome = input("Nome: ").strip().title()
+    email = validaEmail()
+    senha, code = pwd()
+    celular = formatarCell()
+    clear_console()
+    separador(30, 3)
+    centralizar("Especialidade!", 60)
+    separador(30, 3)
+    especialidade = obter_especialidade()
+
+    crud.insert_medico(crm, nome, senha, email, celular, especialidade)
+    atualizacao_txt("Médico cadastrado", crm)
+
+
 def menu_principal():
     option = validacao(1)
     clear_console()
@@ -354,17 +533,22 @@ def menu_principal():
                 option, login, email = login_user()
 
             case 2:
-                pass
+                cadastro_paciente()
+
 
             case 3:
-                pass
+                cadastro_medico()
 
             case 4:
-                url = "../html/hospitais.html"
-                webbrowser.open(url)
-                print("Vermelho: Sua localização\nAzul: Clínicas Notredame\nRoxo: Clínicas HapVida\nLaranja: Hospitais\nVermelho escuro: Imagem e Laboratório\nCinza: Pronto Atendimento\nVerde: Outros")
-                
-                option = validacao(1)
+                if login == False:
+                    print("É necessário estar logado!")
+                    option = 1
+                else:
+                    url = "C:\FIAP\Computational Thinking Using Python\Global-Solution\Global-Solution\html\todos.html"
+                    webbrowser.open(url)
+                    print("Vermelho: Sua localização\nAzul: Clínicas Notredame\nRoxo: Clínicas HapVida\nLaranja: Hospitais\nVermelho escuro: Imagem e Laboratório\nCinza: Pronto Atendimento\nVerde: Outros")
+                    
+                    option = validacao(1)
 
             case 5:
                 url = "https://www.hapvida.com.br/site/politicas-de-privacidade:~:text=A%20Hapvida%20toma%20providências%20técnicas,não%20é%20acessível%20ao%20público."
@@ -380,6 +564,7 @@ def menu_principal():
                 break
 
 def principal():
+    crud.create()
     clear_console()
     menu_principal()
 
